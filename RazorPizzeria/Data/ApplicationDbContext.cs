d2a260cd6108e0eec97c8fdc345094f2adeaffc5
mugokevin <kevinmugo4@gmail.com>
2022-11-18 12:57:08 +0300

    initial commit

---
+++ RazorPizzeria/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<PizzaOrder> pizzaOrders { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+    }
+}
